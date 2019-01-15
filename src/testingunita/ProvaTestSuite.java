ec194a8335b268516cd68bd4e1f05056b2271798
Antonio <Antonio@172.19.6.1>
2019-01-15 01:09:36 +0100

    Caricato tutto

--- src/testingunita/ProvaTestSuite.java
+++ src/testingunita/ProvaTestSuite.java
@@ -5,17 +5,17 @@ import junit.framework.TestSuite;
 import junit.textui.TestRunner;
 
 public class ProvaTestSuite {
-	public static void main(String[] args) {
-		ResetDataBase reset = new ResetDataBase();
-		reset.resetDataBase();
-		TestRunner.run(ProvaTestSuite.suite());
-	}
+  public static void main(String[] args) {
+    ResetDataBase reset = new ResetDataBase();
+    reset.resetDataBase();
+    TestRunner.run(ProvaTestSuite.suite());
+  }
 
-	public static Test suite() {
-		TestSuite test=new TestSuite();
-		test.addTest(TestGIGP.suite());
-		test.addTest(TestGUGp.suite());
-		test.addTest(TestGDGP.suite());
-		return test;
-	}
+  public static Test suite() {
+    TestSuite test = new TestSuite();
+    test.addTest(TestGIGP.suite());
+    test.addTest(TestGUGp.suite());
+    test.addTest(TestGDGP.suite());
+    return test;
+  }
 }
